638e4639b6e44b02a3677ac32ad172d2294d40be
Ashfak Niloy <ashfakniloy56@gmail.com>
2024-01-15 17:15:39 +0600

    added token example

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// example
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const session = await getServerSession(req, res, authOptions);
+  // console.log("session", session);
+
+  const token = session?.user?.token;
+  console.log("token", token);
+
+  res.status(200).json({ name: "John Doe" });
 }
