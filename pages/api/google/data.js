fe68638513c7566bd6e1aba297ee30d798f75530
Yeasir Ahamed <yahamed007@gmail.com>
2024-01-20 03:44:02 +0600

    refactor data.js

--- pages/api/google/data.js
+++ pages/api/google/data.js
@@ -1,6 +1,5 @@
 import { google } from 'googleapis';
-// import fs from "fs";
-import { API_URL } from '@/config';
+import axios from 'axios';
 
 const oauth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
@@ -8,43 +7,47 @@ const oauth2Client = new google.auth.OAuth2(
   process.env.REDIRECT_URI
 );
 
-// function getCredentials() {
-//   try {
-//     const creds = fs.readFileSync("creds.json");
-//     oauth2Client.setCredentials(JSON.parse(creds));
-//   } catch (error) {
-//     console.log("No creds found");
-//   }
-// }
-
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    try {
-      const resolve = await fetch(`${API_URL}/exchange-token`);
-      if (resolve.status === 200) {
-        const tokenData = await resolve.json();
-        if (tokenData.tokens.length) {
-          oauth2Client.setCredentials(tokenData.tokens[0]);
-          const searchConsole = google.searchconsole({
-            version: 'v1',
-            auth: oauth2Client,
-          });
-          const { data } = await searchConsole.searchanalytics.query({
-            siteUrl: 'https://thecatflix.com',
-            startDate: '2023-12-12',
-            endDate: '2024-01-10',
-            dimensions: ['date'],
-          });
-          return res.status(200).json(data);
-        }
-        return res.status(200).json({ error: 'Error executing request' });
-      }
-
-      // return NextResponse.json(data);
-    } catch (error) {
-      console.error('Error executing request', error);
-      return res.status(200).json({ error: 'Error executing request' });
-      // return NextResponse.json({ error: 'Error executing request' });
-    }
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
   }
+
+  try {
+    const tokenData = await fetchAccessToken();
+    if (!tokenData.tokens.length) {
+      return res.status(200).json({ error: 'Error executing request' });
+    }
+    oauth2Client.setCredentials(tokenData.tokens[0]);
+    const searchConsole = google.searchconsole({
+      version: 'v1',
+      auth: oauth2Client,
+    });
+
+    const searchAnalyticsData = await fetchSearchAnalyticsData(searchConsole);
+    return res.status(200).json(searchAnalyticsData);
+  } catch (error) {
+    console.error('Error executing request', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+async function fetchAccessToken() {
+  const response = await axios.get(
+    'https://bayshore-backend.vercel.app/exchange-token'
+  );
+  if (response.status !== 200) {
+    throw new Error('Error obtaining access token');
+  }
+  return response.data;
+}
+
+async function fetchSearchAnalyticsData(searchConsole) {
+  const { data } = await searchConsole.searchanalytics.query({
+    siteUrl: 'https://thecatflix.com',
+    startDate: '2023-12-20',
+    endDate: '2024-01-19',
+    dimensions: ['date'],
+  });
+
+  return data;
 }
