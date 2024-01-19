f39b281199885b3b7cd55df10794d1cc2ec8c963
Yeasir Ahamed <yahamed007@gmail.com>
2024-01-19 23:43:20 +0600

    change credential fn

--- pages/api/google/auth.js
+++ pages/api/google/auth.js
@@ -1,6 +1,6 @@
-import { google } from "googleapis";
+import { google } from 'googleapis';
 // import fs from "fs";
-import { API_URL } from "@/config";
+import { API_URL } from '@/config';
 
 const oauth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
@@ -8,100 +8,70 @@ const oauth2Client = new google.auth.OAuth2(
   process.env.REDIRECT_URI
 );
 
-async function getCredentials() {
-  try {
-    // const creds = fs.readFileSync("creds.json");
-    const res = await fetch(`${API_URL}/exchange-token`);
-    const data = res.json();
-
-    if (data.length) {
-      const token = data.tokens[0];
-
-      oauth2Client.setCredentials(token);
-    }
-  } catch (error) {
-    console.log("No creds found");
-  }
-}
-
-getCredentials();
+// async function getCredentials() {
+//   try {
+//     // const creds = fs.readFileSync("creds.json");
+//     const res = await fetch(`${API_URL}/exchange-token`);
+//     const data = await res.json();
+//     if (data.tokens) {
+//       const token = data.tokens[0];
+//       oauth2Client.setCredentials(token);
+//     }
+//   } catch (error) {
+//     console.log('No creds found');
+//   }
+// }
 
 export default async function handler(req, res) {
-  if (req.method === "GET") {
-    // const url = new URL(req.url);
-
-    // const code = url.searchParams.get("code");
+  if (req.method === 'GET') {
     const { code } = req.query;
 
     try {
       const { tokens } = await oauth2Client.getToken(code);
       oauth2Client.setCredentials(tokens);
-      // fs.writeFileSync("creds.json", JSON.stringify(tokens));
       const res = await fetch(`${API_URL}/exhange-token`, {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({ tokenData: tokens }),
       });
 
       // const mainUrl = url.origin;
 
-      const protocol = req.headers["x-forwarded-proto"] || "http";
+      const protocol = req.headers['x-forwarded-proto'] || 'http';
       const host = req.headers.host;
       const origin = `${protocol}://${host}`;
 
-      console.log("origin", origin);
+      console.log('origin', origin);
 
       // Now you can use `oauth2Client` for further API requests
       res.redirect(307, origin);
-      // return NextResponse.redirect(mainUrl);
     } catch (error) {
-      console.error("Error exchanging code for tokens", error.message);
+      console.error('Error exchanging code for tokens', error.message);
       return res
         .status(400)
-        .json({ error: "Error exchanging code for tokens" });
-      // return NextResponse.json({ error: "Error exchanging code for tokens" });
+        .json({ error: 'Error exchanging code for tokens' });
     }
   }
 
-  if (req.method === "POST") {
-    const scopes = [
-      "https://www.googleapis.com/auth/userinfo.profile",
-      "https://www.googleapis.com/auth/webmasters",
-      "https://www.googleapis.com/auth/webmasters.readonly",
-    ];
+  if (req.method === 'POST') {
+    const res = await fetch(`${API_URL}/exchange-token`);
+    if (res.status === 200) {
+      const tokenData = await res.json();
+      oauth2Client.setCredentials(tokenData.tokens[0]);
+      const scopes = [
+        'https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/webmasters',
+        'https://www.googleapis.com/auth/webmasters.readonly',
+      ];
 
-    const url = oauth2Client.generateAuthUrl({
-      access_type: "offline",
-      scope: scopes,
-    });
+      const url = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: scopes,
+      });
 
-    return res.status(200).json({ url });
-    // return NextResponse.json({ url });
+      return res.status(200).json({ url });
+    }
   }
 }
-
-// import { getServerSession } from "next-auth";
-// import { authOptions } from "../auth/[...nextauth]";
-// import { authConfigure } from "@/utils/authConfigure";
-
-// export default async function handler(req, res) {
-//   const session = await getServerSession(req, res, authOptions);
-//   const token = session?.user?.token;
-//   const oauth2Client = authConfigure(token);
-//   // console.log("session", session);
-
-//   const scopes = [
-//     "https://www.googleapis.com/auth/userinfo.profile",
-//     "https://www.googleapis.com/auth/drive",
-//   ];
-
-//   const url = oauth2Client.generateAuthUrl({
-//     access_type: "offline",
-//     scope: scopes,
-//   });
-
-//   // res.redirect(307, url)
-//   res.redirect(url);
-// }
--- pages/api/google/data.js
+++ pages/api/google/data.js
@@ -1,6 +1,6 @@
-import { google } from "googleapis";
+import { google } from 'googleapis';
 // import fs from "fs";
-import { API_URL } from "@/config";
+import { API_URL } from '@/config';
 
 const oauth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
@@ -17,43 +17,30 @@ const oauth2Client = new google.auth.OAuth2(
 //   }
 // }
 
-async function getCredentials() {
-  try {
-    // const creds = fs.readFileSync("creds.json");
-    const res = await fetch(`${API_URL}/exchange-token`);
-    const data = res.json();
-
-    if (data.length) {
-      const token = data.tokens[0];
-
-      oauth2Client.setCredentials(token);
-    }
-  } catch (error) {
-    console.log("No creds found");
-  }
-}
-
-getCredentials();
-
 export default async function handler(req, res) {
-  if (req.method === "GET") {
-    const searchConsole = google.searchconsole({
-      version: "v1",
-      auth: oauth2Client,
-    });
-
+  if (req.method === 'GET') {
     try {
-      const { data } = await searchConsole.searchanalytics.query({
-        siteUrl: "https://thecatflix.com",
-        startDate: "2023-12-12",
-        endDate: "2024-01-10",
-        dimensions: ["date"],
-      });
-      return res.status(200).json(data);
+      const res = await fetch(`${API_URL}/exchange-token`);
+      if (res.status === 200) {
+        const tokenData = await res.json();
+        oauth2Client.setCredentials(tokenData.tokens[0]);
+        const searchConsole = google.searchconsole({
+          version: 'v1',
+          auth: oauth2Client,
+        });
+        const { data } = await searchConsole.searchanalytics.query({
+          siteUrl: 'https://thecatflix.com',
+          startDate: '2023-12-12',
+          endDate: '2024-01-10',
+          dimensions: ['date'],
+        });
+        return res.status(200).json(data);
+      }
+
       // return NextResponse.json(data);
     } catch (error) {
-      console.error("Error executing request", error);
-      return res.status(200).json({ error: "Error executing request" });
+      console.error('Error executing request', error);
+      return res.status(200).json({ error: 'Error executing request' });
       // return NextResponse.json({ error: 'Error executing request' });
     }
   }
